139f214bbd31d7b7d24b7fb3299ddf8c273af445
saadahmedbd <saadahmedbd0@gmail.com>
2024-10-27 11:11:04 +0600

    make localstoragekey and loadfrom stroage private

--- data/cart-class.js
+++ data/cart-class.js
@@ -1,14 +1,14 @@
 class Cart {
     cartItems;
-    localStorageKey;
+    #localStorageKey;//private method
 
     constructor(localStorageKey){
-        this.localStorageKey=localStorageKey;
-        this.loadFromStorage();
+        this.#localStorageKey=localStorageKey;
+        this.#loadFromStorage();
     }
 
-    loadFromStorage(){
-        this.cartItems=JSON.parse(localStorage.getItem(this.localStorageKey));
+    #loadFromStorage(){
+        this.cartItems=JSON.parse(localStorage.getItem(this.#localStorageKey));
     
         if(!this.cartItems){
             this.cartItems=[{
@@ -27,7 +27,7 @@ class Cart {
     
     };
     saveToStroage(){
-        localStorage.setItem(this.localStorageKey,JSON.stringify(this.cartItems));
+        localStorage.setItem(this.#localStorageKey,JSON.stringify(this.cartItems));
     };
 
     
@@ -82,7 +82,6 @@ class Cart {
 
 const cart =new Cart('cart-oop');
 const busienssCart =new Cart('cart-busienss');
-
 
 
 // cart.addToCart('54e0eccd-8f36-462b-b68a-8182611d9add');
